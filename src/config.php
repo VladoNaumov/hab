609f975c40e0d6e89349beefe5b7c5b40873922a
umbre <umbrella.spb@hotmail.com>
2025-06-18 18:24:29 +0300

    refactoring

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('API_KEY', 'RrVdKyDNBaJH6lCo33MBZjREnd03dQZj');
-define('API_SECRET', 'HTqBHPrhCLcTx0KI');
-define('MERCHANT_ID', 'ccb2c181-56eb-4283-bb4d-b24961ae8dac');
-define('BASE_URL', 'https://api.sandbox.op.fi/payments/v1'); // Возвращаем домен
-define('RETURN_URL', 'https://www.encanta.fi/op-payment/callback.php?type=success');
-define('CANCEL_URL', 'https://www.encanta.fi/op-payment/callback.php?type=cancel');
-define('NOTIFICATION_URL', 'https://www.encanta.fi/op-payment/callback.php?type=notify');
-?>
